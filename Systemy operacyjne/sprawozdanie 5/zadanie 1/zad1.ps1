8eb71c0b8d92fde965c85380eb657af929e7acae
Enemon <LockerWarUA@gmail.com>
2020-05-16 16:08:08 +0200

    sprawozdanie 5(PowerShell)

---
+++ Systemy operacyjne/sprawozdanie 5/zadanie 1/zad1.ps1
@@ -0,0 +1,9 @@
+﻿Param (
+[string]$dir,
+[string]$count
+)
+
+New-Item -Path $dir -ItemType Directory
+for ($i = 1; $i -le $count; $i++){
+    New-Item -Path $dir\$i.txt -ItemType file
+}
---
+++ Systemy operacyjne/sprawozdanie 5/zadanie 2/zad2.ps1
@@ -0,0 +1,14 @@
+﻿Param (
+[string]$path,
+[string]$Symbol
+)
+$count=0
+$fil=[System.IO.File]::ReadAllText($path)
+$len = $fil.Length
+for ($i=0; $i -le $len; $i++){
+    if ($fil[$i] -match $Symbol){
+        $count++;
+    }
+}
+
+$count
